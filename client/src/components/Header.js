e85fccb772299dfa754a9b365a17f96af30fda45
Conner Gleason <connereg@optonline.net>
2022-01-26 14:13:20 -0500

    Added stylings for Homepage Content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom"
 
 
 function Header() {
@@ -10,9 +9,43 @@ function Header() {
                 backgroundColor: '#C4C4C4',
             }}
         >
-            <Link to="/">
-                <h1 classname="application-title">Page Heading Here</h1>
-            </Link>
+            <text 
+                style={{
+                    position: 'absolute',
+                    height: '152px',
+                    left: '10.42%',
+                    right: '40.97%',
+                    //top: 'calc(50% - 152px/2 - 109.5px)',
+                    top: '114px',
+                    width: '700px',
+                    
+                    fontStyle: 'normal',
+                    fontWeight: 'normal',
+                    fontSize: '66px',
+                    lineHeight: '76px',
+                    color: '#FFFFFF',
+                    fontFamily: 'Ubu'
+
+                }}
+            >
+                Necessitie
+                Full Service Software Development
+            </text>
+            <text style={{
+                position: 'absolute',
+                height: '85px',
+                width: '576px',
+                left: '10.45%',
+                top: '400px',
+
+
+                fontFamily: 'Ubu',
+                color: '#FFFFFF',
+                fontSize: '17px',
+                lineHeight: '24px'
+                }}>
+            More stuff about our services and goals, a catchy little thing to hear, made for you by us
+            </text>
         </div>
     );
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,12 +1,63 @@
+import { fontStyle } from '@mui/system';
 import React from 'react';
 
 function Homepage(props) {
     return (
     <div>
         <h3>HOMEPAGE</h3>
-        <text className="homeblurb">You share your idea. <br/> We get it done</text>
+        <text 
+        style={{
+            position: 'absolute',
+            width: '490px',
+            height: '114px',
+            left: '140px',
+            top: '680px',
+
+            fontFamily: 'Ubumedium',
+            fontSize: '50px',
+            lineHeight: '57px',
+            color: '#424B5A'
+
+        }}
+        >
+            You share your idea. We get it done.
+        </text>
+        <text
+        style={{
+            position: 'absolute',
+            width: '590px',
+            height: '144px',
+            left: '820px',
+            top: '680px',
+            
+            fontFamily: 'Ubu',
+            fontStyle: 'normal',
+            fontWeight: 'normal',
+            fontSize: '18px',
+            lineHeight: '24px',
+            color: '#A1AEB7'
+        }}
+        >
+            OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA OWARI DA
+        </text>
+        <text
+        style={{
+            position: 'relative',
+            width: '700px',
+            height: '114px',
+            textAlign: 'center',
+            top: '610px',
+
+            fontFamily: 'Ubumedium',
+            fontSize: '50px',
+            lineHeight: '57px',
+            color: '#424B5A'
+        }}>
+        See what we can do for you (INFO ICONS HERE WITHS DESCRIPTIONS)
+        </text>
     </div>
     )
 }
 
 export default Homepage;
+
