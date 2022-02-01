e11d0a5fd47b2e2159956e0e7b22649c5e444add
anthony chimiak <anthony@knockmedia.com>
2022-02-01 21:03:48 +0100

    moved navbar and added

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import TemporaryDrawer from './TemporaryDrawer'
+
+import './navbar.scss';
+
+export const NavBar = () => {
+    return (
+    <div 
+        className="navbar"
+        // style={{
+        //   padding: "20px",
+        //   backgroundColor: "white"
+        // }}
+    >        
+        <nav
+            style={{
+                float: "right"
+            }}
+        >
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/services">Services</Link> |{" "}
+        <Link to="/technologies">Technologies</Link> |{" "}
+        <Link to="/ContactUs">Contact Us</Link>
+        {/* <TemporaryDrawer /> */}
+      </nav>
+    </div>
+    )
+}
+
+export default NavBar;
