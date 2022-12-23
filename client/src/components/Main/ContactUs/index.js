703a4b420720f856fe4ca1102759011c269ef553
anthony chimiak <anthony@knockmedia.com>
2022-12-23 18:39:40 -0500

    adding file

---
+++ client/src/components/Main/ContactUs/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ContactUs from '../ContactUs/ContactUs';
+
+
+
+
+function Contact(props) {
+
+  return (
+    <div className='contact main-content'>
+      <div className="contact-page page">
+        <ContactUs />
+      </div>
+    </div>
+  )
+}
+
+export default Contact;
