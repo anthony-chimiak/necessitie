f14b0de90b24705a748149dff683ebb7c3523c69
anthony chimiak <anthony@knockmedia.com>
2023-01-14 02:02:36 -0500

    reverting

---
+++ client/src/components/Main/Map/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Button} from 'semantic-ui-react';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+// import LocationOnIcon from '@mui/icons-material/LocationOn';
+// import PhoneIcon from '@mui/icons-material/Phone';
+// import EmailIcon from '@mui/icons-material/Email';
+
+import ContactUs from '../ContactUs/ContactUs.js';
+import { ForceGraph2D } from "react-force-graph";
+
+//IMAGES
+// import landingPic from "./../../../assets/images/landing-guy.png"
+import lodgiqEx from "./../../../assets/images/Lodgiq-example.png"
+import medidataEx from "./../../../assets/images/Medidata-example.png"
+import twineEx from "./../../../assets/images/twine-example.png"
+import landingImg from "./../../../assets/images/home-landing.png" 
+import rocketImg from "./../../../assets/images/rocket.png" 
+import houseImg from "./../../../assets/images/industry-house.png" 
+import crossImg from "./../../../assets/images/industry-cross.png" 
+import pyramidImg from "./../../../assets/images/industry-pyramid.png" 
+import slabImg from "./../../../assets/images/industry-slab.png" 
+import sittingDudeImg from "./../../../assets/images/sitting-dude.png"
+import atomImg from "./../../../assets/images/atom.png"
+import servicesMainImg from "./../../../assets/images/services.png"
+import servicesReactImg from "./../../../assets/images/Crypto/dent.svg"
+import servicesPythonImg from "./../../../assets/images/Crypto/python.svg"
+import servicesUxImg from "./../../../assets/images/element-equal.svg"
+import servicesQaImg from "./../../../assets/images/Search/search-normal.svg"
+import androidImg from "./../../../assets/images/Platform/android.svg"
+import iosImg from "./../../../assets/images/Platform/apple.svg"
+import frontendImg from "./../../../assets/images/Platform/frontend.svg"
+import backendImg from "./../../../assets/images/Platform/message-programming.svg"
+import crossPlatformImg from "./../../../assets/images/Platform/cross-platform.svg"
+import aiImg from "./../../../assets/images/Platform/ai.svg"
+import industriesFullImg from "./../../../assets/images/allcubes.png"
+
+import NetworkCluster from './Cluster/clusterTest2.jsx';
+
+import './map.scss'
+
+export const Home = (props) => {
+    return (
+        <div className="map main-content">
+            <div className="cluster-page page">
+                <NetworkCluster/>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
+
